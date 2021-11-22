94b668deb660eff78c062a5fd76b71568de93a67
best-jinny <iwilldotherightthings@gmail.com>
2021-11-23 08:45:00 +0900

    직렬화 에러 수정

--- src/main/java/gg/config/auth/CustomOAuth2UserService.java
+++ src/main/java/gg/config/auth/CustomOAuth2UserService.java
@@ -23,6 +23,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         private final UserRepository userRepository;
         private final HttpSession httpSession;
 
+        @Override
         public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
                 OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
                 OAuth2User oAuth2User = delegate.loadUser(userRequest);
--- src/main/java/gg/config/auth/dto/SessionUser.java
+++ src/main/java/gg/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package gg.config.auth.dto;
 import gg.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;

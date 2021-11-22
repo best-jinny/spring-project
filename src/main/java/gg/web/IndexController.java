f689a75d8ae66cb7b25628c4e5401896a94dabc9
best-jinny <iwilldotherightthings@gmail.com>
2021-11-22 11:02:33 +0900

    구글 로그인 - 어노테이션 방식으로 개선

--- src/main/java/gg/web/IndexController.java
+++ src/main/java/gg/web/IndexController.java
@@ -1,5 +1,6 @@
 package gg.web;
 
+import gg.config.auth.LoginUser;
 import gg.config.auth.dto.SessionUser;
 import gg.service.posts.PostsService;
 import gg.web.dto.PostsResponseDto;
@@ -16,13 +17,13 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    //private final HttpSession httpSession;
 
-    @GetMapping("/")
-    public String index(Model model) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
+    @GetMapping("/") // 어느 컨트롤러든지 @LoginUser 를 사용해 세션 정보를 가져 올 수 있음
+    public String index(Model model, @LoginUser SessionUser user) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음.
         model.addAttribute("posts", postsService.findAllDesc());
-        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
-        SessionUser user = (SessionUser) httpSession.getAttribute(("user"));
+        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장 -> @LoginUser 어노테이션으로 개선
+        //SessionUser user = (SessionUser) httpSession.getAttribute(("user"));
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }

c53a036d1f26ceac684a6a2fbe74faff225be619
best-jinny <iwilldotherightthings@gmail.com>
2021-11-23 15:43:42 +0900

    5.7 기존 테스트에 시큐리티 적용하기

--- src/test/java/gg/web/HelloControllerTest.java
+++ src/test/java/gg/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package gg.web;
 
+import gg.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -23,13 +27,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 선언할 경우 @Controller, @ControllerAdvice 등을 사용할 수 있음
 // 단, @Service, @Component, @Repository 등은 사용할 수 없음
 // 여기서는 컨트롤러만 사용하기 때문에 선언.
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈(Bean) 주입 받음
     private MockMvc mvc; // 웹 API를 테스트할 때 사용, 스프링 MVC테스트의 시작점, 이 클래스를 통해 HTTP GET, POST 등에 대한 API테스트를 할 수 있음
 
+    @WithMockUser(roles="USER")
     @Test
     public void return_hello() throws Exception {
         String hello = "hello";
@@ -39,6 +47,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // 응답 본문의 내용을 검증. 컨트롤러에서 hello를 리턴하기 때문에 이 값이 맞는지 검증
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
